12c0dc1793ba9f01ea47b58ee557e5b5e0f7a097
Wiiya <wii.eng14@gmail.com>
2025-03-28 00:52:02 +0700

    Update calculation total worth of leader board
    - Update calculation total worth of leader board by filter sold fish.
    - find the top price fish to show

--- src/app/Http/Controllers/LeaderBoardController.php
+++ src/app/Http/Controllers/LeaderBoardController.php
@@ -12,18 +12,30 @@ class LeaderBoardController extends Controller
     public function index()
     {
         $leaderboard = FishRecord::join('Fish', 'FishRecord.FishID', '=', 'Fish.FishID')
-            ->select('FishRecord.FisherID', DB::raw('SUM(Fish.FishWorth) as total_worth'))
-            ->groupBy('FishRecord.FisherID')
-            ->orderBy('total_worth', 'desc')
-            ->take(5)
-            ->get();
+                                ->select('FishRecord.FisherID', DB::raw('SUM(Fish.FishWorth) as total_worth'))
+                                ->whereNull('FishRecord.FishSoldDate')
+                                ->groupBy('FishRecord.FisherID')
+                                ->orderBy('total_worth', 'desc')
+                                ->take(5)
+                                ->get();
 
         // ดึงชื่อผู้เล่นจากตาราง users
         $leaderboard = $leaderboard->map(function ($record) {
             $user = User::find($record->FisherID);
+
+            $top_fish = FishRecord::join('Fish', 'FishRecord.FishID', '=', 'Fish.FishID')
+                                    ->selectRaw('FishRecord.FishID, Fish.FishName, MAX(Fish.FishWorth) as FishWorth')
+                                    ->whereNull('FishRecord.FishSoldDate')
+                                    ->where('FishRecord.FisherID', $record->FisherID)
+                                    ->groupBy('FishRecord.FishID')
+                                    ->orderBy('Fish.FishWorth', 'desc')
+                                    ->take(3)
+                                    ->get();
+
             return [
                 'name' => $user->username,
-                'total_worth' => $record->total_worth
+                'total_worth' => $record->total_worth,
+                'top_fish' => $top_fish,
             ];
         });
 
